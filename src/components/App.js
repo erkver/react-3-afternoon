d1266a4c46ec8256af0144ab98c347641e4466f5
Eric Vere <erkver250@gmail.com>
2018-09-11 18:05:21 -0500

    Completed react-3-afternoon project

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-
+import axios from "axios";
 import './App.css';
+import Post from './Post/Post'
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+const BASE_URL = 'https://practiceapi.devmountain.com/api';
+
 
 class App extends Component {
   constructor() {
@@ -16,22 +19,43 @@ class App extends Component {
     this.updatePost = this.updatePost.bind( this );
     this.deletePost = this.deletePost.bind( this );
     this.createPost = this.createPost.bind( this );
+    this.filterPost = this.filterPost.bind( this );
   }
   
   componentDidMount() {
-
+    axios.get(BASE_URL + '/posts').then(res => {
+      console.log("res: ", res)
+      this.setState({posts: res.data});
+    }).catch(err => console.log(err));
   }
 
-  updatePost() {
-  
+  updatePost(id, text) {
+    axios.put(`${BASE_URL}/posts?id=${ id }`, {text}).then(res => {
+      console.log("res: ", res);
+      this.setState({posts: res.data});
+    }).catch(err => console.log(err));
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    axios.delete(`${BASE_URL}/posts?id=${ id }`).then(res => {
+      console.log("res: ", res);
+      this.setState({posts: res.data})
+    }).catch(err => console.log(err))
   }
 
-  createPost() {
+  createPost(text) {
+    axios.post(BASE_URL + '/posts', {text}).then(res => {
+      console.log("res: ", res);
+      this.setState({posts: res.data})
+    }).catch(err => console.log(err))
+  }
 
+  filterPost() {
+    let str = this.str.value;
+    axios.get(BASE_URL + '/posts', {str}).then(res => {
+      console.log("res: ", res);
+      this.setState({posts: res.data});
+    }).catch(err => console.log(err))
   }
 
   render() {
@@ -39,12 +63,27 @@ class App extends Component {
 
     return (
       <div className="App__parent">
-        <Header />
+        <Header 
+          filterPostFn={this.filterPost}
+          ref={str => this.str = str}
+        />
 
         <section className="App__content">
 
-          <Compose />
-          
+          <Compose 
+            createPostFn={this.createPost}
+          />
+          {posts.map(e => (
+            <Post 
+              key={e.id} 
+              text={e.text}
+              date={e.date}
+              updatePostFn={this.updatePost}
+              id={e.id}
+              deletePostFn={this.deletePost}
+            />
+            ))
+          }
         </section>
       </div>
     );
--- src/components/Header/Search/Search.js
+++ src/components/Header/Search/Search.js
@@ -7,14 +7,32 @@ import SearchIcon from 'react-icons/lib/md/search';
 //////////////////////////////////////////////////////// THIS COMPONENT IS BEING RENDERED IN THE *HEADER* COMPONENT
 
 export default class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchInput: "",
+      filteredPosts: []
+    }
+    this.handleChange = this.handleChange.bind( this );
+  }
+
+  handleChange(e) {
+    this.setState({searchInput: e.target.value});
+  }
+
+  getNewPosts() {
+    this.props.filterPostFn(this.state.searchInput);
+    this.setState({searchInput: ""})
+  }
+
   render() {
     return (
       <section className="Search__parent">
 
         <div className="Search__content">
-          <input placeholder="Search Your Feed" />
-
-          <SearchIcon id="Search__icon" />
+          <input placeholder="Search Your Feed" onChange={e => this.handleChange(e)} />
+          
+          <SearchIcon id="Search__icon" onClick={this.getNewPosts} />
         </div>
         
       </section>
